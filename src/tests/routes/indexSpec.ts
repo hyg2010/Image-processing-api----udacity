6c8fa20bd52b684b4b7ecec61fb4edb6fec156de
hyg2010 <81759800+hyg2010@users.noreply.github.com>
2021-09-08 18:33:50 -0700

    Update indexSpec.ts

--- src/tests/routes/indexSpec.ts
+++ src/tests/routes/indexSpec.ts
@@ -1,3 +1,4 @@
+import req from 'express/lib/request';
 import supertest from 'supertest';
 import app from '../../index';
 import imageResize from '../../utilities/imageResize';
@@ -5,23 +6,11 @@ import imageResize from '../../utilities/imageResize';
 //test api route
 const request = supertest(app);
 
+//test for successful resized endpoint with query parameters
 
-
-//test for successful resized image
 it('tesing endpoint for resized images', async () => {
-    const response = await request.get('/api/images');
+    const response = await request.get('/api/images?filename=fjord.jpg&width=200&height=200');
     expect(response.status).toBe(200);
   });
   
 
-
- //Image width + height resize to be greater than 0
-
- it('expects Resized Image result to be great than 0', async () => {
-    const fileName = 'fjord.jpg';
-    const width = 200;
-    const height = 200;
-    return imageResize().then ( result => {
-        expect(imageResize).toBeGreaterThan(0);
-    })
- });
